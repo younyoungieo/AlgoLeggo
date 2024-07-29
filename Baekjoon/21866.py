257900db363787b3731e84335f7dab37b417bdd2
younyoung <00younyoung@naver.com>
2024-07-29 17:54:26 +0900

    문제별 설명 추가

--- Baekjoon/21866.py
+++ Baekjoon/21866.py
@@ -1,3 +1,9 @@
+"""
+* 문제: https://www.acmicpc.net/problem/21866
+    경진대회 점수 결과를 받아서, 해커인지, 추첨 대상자인지, 아무것도 아닌지를 출력하는 프로그램을 작성하시오.
+* 풀이: 각 점수에 대한 최대 점수를 리스트로 만들어서, zip 함수를 이용하여 각 점수와 최대 점수를 비교함.
+"""
+
 grades_str = input()
 grades_str_list = grades_str.split(" ")
 grades = list(map(int, grades_str_list))
